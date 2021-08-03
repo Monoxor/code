160e646743795e307c963234f21718d5420a0fe4
PankajMoolrajani <pmoolrajani@gmail.com>
2021-08-03 03:27:50 +0000

    removing images from summary card

--- src/apps/tech-conferences/src/components/ConferenceSummary.component.js
+++ src/apps/tech-conferences/src/components/ConferenceSummary.component.js
@@ -10,7 +10,7 @@ function ConferenceSummary(props) {
     <Box id='conference-card' 
       style={{
         display: 'flex', flexDirection: 'row', backgroundColor: 'white', borderRadius: 4,
-        height: 200, marginTop: 30
+        height: 100, marginTop: 30, padding: 20
       }}
     >
       {/* <Box id='conference-img' style={{display: 'flex', flex: 1, backgroundColor: 'lightgrey', margin: 20, borderRadius: 8}}>
@@ -24,18 +24,18 @@ function ConferenceSummary(props) {
         </Box>
       </Box> */}
       <Box id='conference-summary' 
-        style={{flex: 3, display: 'flex', flexDirection: 'column', marginTop: 20, marginBottom: 20}}
+        style={{flex: 3, display: 'flex', flexDirection: 'column'}}
       >
         <Box style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
           <Box style={{fontSize: 20, fontWeight: 'bold'}}>{props.conference.name}</Box>
-          <Box style={{display: 'flex', flexDirection: 'row', fontSize: 14, marginRight: 20}}>
-            <Box style={{paddingRight: 15}}>Aug 8 to Aug 11</Box>
+          <Box style={{display: 'flex', flexDirection: 'row', fontSize: 14}}>
+            <Box>Aug 8 to Aug 11</Box>
           </Box>
         </Box>
-        <Box style={{flex: 1, marginTop: 15, marginRight: 20, fontSize: 12, fontStyle: 'italic'}}>
+        <Box style={{flex: 1, marginTop: 15, fontSize: 12, fontStyle: 'italic'}}>
           {props.conference.description}
         </Box>
-        <Box style={{marginRight: 20, alignItems: 'space-around', }}>
+        <Box style={{marginRight: 20, marginTop: 10, alignItems: 'space-around', }}>
           <Chip style={{marginRight: 10}} label='Security' size='small'/>
           <Chip style={{marginRight: 10}} label='Hacking' size='small'/>
         </Box>
