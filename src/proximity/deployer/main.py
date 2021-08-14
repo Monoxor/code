8efa60f7178267699f840a09ee1f8970414c7f33
PankajMoolrajani <pmoolrajani@gmail.com>
2021-08-14 01:56:14 +0000

    proximity deployer baseline code - not finished

--- src/proximity/deployer/main.py
+++ src/proximity/deployer/main.py
@@ -4,7 +4,15 @@ from kubernetes import client, config
 config.load_kube_config()
 
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+#container
+container = client.V1Container(
+  name='busybox',
+  image='busybox',
+)
+#template
+template = client.V1PodTemplateSpec(
+    metadata=client.V1ObjectMeta(labels={'name': 'simple-job'}),
+    spec=client.V1PodSpec(restart_policy='OnFailure', containers=[container]))
+#spec
+spec = client.V1JobSpec(template=template)
