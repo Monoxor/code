321af2ec3f40d8558adfa16ef4fca2167b2208ab
PankajMoolrajani <pmoolrajani@gmail.com>
2021-08-28 17:49:08 +0000

    now pagination working with conferences endpoint, need to handle last page end of record error

--- src/confdaddy/web/app/pages/posts.js
+++ src/confdaddy/web/app/pages/posts.js
@@ -4,12 +4,20 @@ import Post from './../components/blog/Post'
 
 
 
-function Posts ({data}) {
+function Posts ({data, pagination}) {
     const [posts, setPosts] = useState(data)
+    const [hasMore, setHasMore] = useState(true)
+    const [pageNum, setPageNum] = useState(1)
     const getMorePosts = async() => {
-        const res = await fetch('https://confdaddy-services-i7cau.ondigitalocean.app/service/confdaddy/conferences?page_num=2')
+        const res = await fetch(`https://confdaddy-services-i7cau.ondigitalocean.app/service/confdaddy/conferences?page_num=${pageNum}`)
         const newPosts = (await res.json()).data
         setPosts(posts => [...posts, ...newPosts])
+        if (pagination.next_page_exists) {
+            let newHasMore = true
+            setHasMore(newHasMore)
+        }
+        let newPageNum = pageNum + 1
+        setPageNum(newPageNum)
     }
     return (
         <main>
@@ -35,11 +43,14 @@ export async function getServerSideProps() {
     const API_URL = 'https://confdaddy-services-i7cau.ondigitalocean.app/service/confdaddy/conferences?page_num=1'
     const res = await fetch(API_URL)
     let data = await res.json()
+    let pagination = data.pagination
+    pagination['page_num'] = 1
     data = data.data
 
     return {
         props: {
-            data
+            data: data,
+            pagination
         }
     }
 }
